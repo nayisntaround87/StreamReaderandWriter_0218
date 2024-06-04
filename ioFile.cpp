d82b2b21cdddda15e141e512a11554cb48447363
nayisntaround87 <naylasalwahayati@gmail.com>
2024-06-04 14:54:29 +0700

    jika tidak ditemukan file maka akan menampilkan ini     else cout << "unable to open file";     return 0;

--- ioFile.cpp
+++ ioFile.cpp
@@ -33,5 +33,20 @@ int main()
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
 
-    cout << endl << ">= membuka dan membaca file"
+    cout << endl << ">= membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "unable to open file";
+    return 0;
 }
