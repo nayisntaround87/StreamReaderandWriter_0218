ac864c357b08f54c38ca81c5806eb49081c3b8b3
nayisntaround87 <naylasalwahayati@gmail.com>
2024-06-04 15:13:18 +0700

    menambahkan catch dan try

--- exceptionStandar.cpp
+++ exceptionStandar.cpp
@@ -7,5 +7,20 @@ using namespace std;
 
 int main()
 {
-    cout
+    cout << "Awal Program" << endl; //penanda 1:...
+    try
+    {
+        array<int, 3> data = { 3, 5, 7};
+        //pesan array integer 3 elemen
+        cout << data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
+    catch (exception& e)
+    {
+        // penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan di eksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    
+    
 }
