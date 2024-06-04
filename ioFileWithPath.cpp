143fcaacfaa4a01f52861604b6f6c574e337b5c9
nayisntaround87 <naylasalwahayati@gmail.com>
2024-06-04 15:04:09 +0700

    menambahkan infile.open(NamaFile + ".txt", ios::in);

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -34,8 +34,9 @@ int main()
 
     //membuka file dalam mode membaca
     ifstream infile;
+
     //menunjuk ke sebuah file
-    infile.open("contohfile.txt");
+    infile.open(NamaFile + ".txt", ios::in);
 
     cout << endl << ">= membuka dan membaca file " << endl;
     // jika file ada maka
